db3eb42ea8bc844d912fc211c88ebf78418e6a98
andrew <devnull@localhost>
2011-08-01 18:59:46 +0100

    Moved extensions to separate file

--- NSemVer/Visitors/BreakingChanges/BreakingChangeVisitor.cs
+++ NSemVer/Visitors/BreakingChanges/BreakingChangeVisitor.cs
@@ -3,7 +3,6 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
-	using Mono.Cecil;
 
 	public class BreakingChangeVisitor : IChangeVisitor
 	{
@@ -61,15 +60,14 @@
 			{
 				{
 					ApiBreakType.NewInstanceMethod,
-					methodGroupChange => methodGroupChange.GetAllNewMethodChanges().Any(methodChange => methodChange.IsPubliclyVisible())
+					methodGroupChange => methodGroupChange.GetAllNewMethodChanges().Any(methodChange => methodChange.Method.IsPubliclyVisible())
 				},
-
 				{
 				    ApiBreakType.MethodOverloadedWithInterfaceBasedParameter,
 				    methodGroupChange => methodGroupChange.ChangeType == ChangeType.Matched && 
 				                         methodGroupChange.MethodChanges.Any(methodChange => 
 											methodChange.ChangeType == ChangeType.Added &&
-				                            methodChange.IsPubliclyVisible() &&
+				                            methodChange.Method.IsPubliclyVisible() &&
 											methodChange.GetNewParameters(methodGroupChange).Any(x => x.ParameterType.Resolve().IsInterface)
 				                         )
 				},
@@ -100,71 +98,5 @@
 				}
 			}
 		}
-	}
-
-	// I wish there was a way to declare private extension classes :(
-	public static class Extensions
-	{
-		public static bool IsPubliclyVisible(this MethodChange change)
-		{
-			return change.Method.IsPublic && change.Method.DeclaringType.IsPublic;
-		}
-
-		public static IEnumerable<MethodChange> GetAllNewMethodChanges(this MethodGroupChange methodGroupChange)
-		{
-			return methodGroupChange.ChangeType == ChangeType.Added
-			        ? methodGroupChange.MethodChanges
-			        : methodGroupChange.ChangeType == ChangeType.Matched
-			         	? methodGroupChange.MethodChanges.Where(x => x.ChangeType == ChangeType.Added)
-			         	: new MethodChange[0];
-		}
-
-		public static IEnumerable<ParameterDefinition> GetNewParameters(this MethodChange methodChange, MethodGroupChange methodGroupChange)
-		{
-			Tuple<MethodDefinition, ParameterDefinition[]>[] newParametersByOriginalMethod = methodChange.Method.GetNewParametersByOriginalMethod(methodGroupChange);
-
-			return newParametersByOriginalMethod
-				.SelectMany(x => x.Item2)
-				.Distinct(new ParameterDefinitionComparer());
-		}
-
-		public static Tuple<MethodDefinition, ParameterDefinition[]>[] GetNewParametersByOriginalMethod(this MethodDefinition method, MethodGroupChange methodGroupChange)
-		{
-			var existingMethods = methodGroupChange.MethodChanges
-				.Where(x => x.ChangeType == ChangeType.Matched) // Only select methods that are still matched (I.e. that haven't been removed)
-				.Select(x => x.Method);
-
-			var methodParameterSet = new HashSet<ParameterDefinition>(method.Parameters);
-
-			return existingMethods
-				.Select(existingMethod => Tuple.Create(
-					existingMethod,
-					methodParameterSet.Except(new HashSet<ParameterDefinition>(existingMethod.Parameters), new ParameterDefinitionComparer()).ToArray()))
-				.ToArray();
-		}
-
-		private class ParameterDefinitionComparer : IEqualityComparer<ParameterDefinition>
-		{
-			public bool Equals(ParameterDefinition x, ParameterDefinition y)
-			{
-				return x.ParameterType.FullName == y.ParameterType.FullName;
-			}
-
-			public int GetHashCode(ParameterDefinition obj)
-			{
-				return obj.ParameterType.FullName.GetHashCode();
-			}
-		}
-
-		/*
-		public static bool OverloadedParametersContains(this MethodChange methodChange, Func<ParameterReference, bool> predicate)
-		{
-
-			var newParameters = methodChange.Method.Parameters
-
-			//return methodChange.Method.DeclaringType.Methods
-			//    .Where(x => x.IsPublic)
-			//    .Any(x => x.Name == methodChange.Method.Parameters.First().ParameterType.);
-		}*/
 	}
 }
---
+++ NSemVer/Visitors/BreakingChanges/Extensions.cs
@@ -0,0 +1,61 @@
+﻿namespace NSemVer.Visitors.BreakingChanges
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Mono.Cecil;
+
+	public static class Extensions
+	{
+		public static bool IsPubliclyVisible(this MethodDefinition method)
+		{
+			return method.IsPublic && method.DeclaringType.IsPublic;
+		}
+
+		public static IEnumerable<MethodChange> GetAllNewMethodChanges(this MethodGroupChange methodGroupChange)
+		{
+			return methodGroupChange.ChangeType == ChangeType.Added
+			       	? methodGroupChange.MethodChanges
+			       	: methodGroupChange.ChangeType == ChangeType.Matched
+			       	  	? methodGroupChange.MethodChanges.Where(x => x.ChangeType == ChangeType.Added)
+			       	  	: new MethodChange[0];
+		}
+
+		public static IEnumerable<ParameterDefinition> GetNewParameters(this MethodChange methodChange, MethodGroupChange methodGroupChange)
+		{
+			Tuple<MethodDefinition, ParameterDefinition[]>[] newParametersByOriginalMethod = methodChange.Method.GetNewParametersByOriginalMethod(methodGroupChange);
+
+			return newParametersByOriginalMethod
+				.SelectMany(x => x.Item2)
+				.Distinct(new ParameterDefinitionComparer());
+		}
+
+		public static Tuple<MethodDefinition, ParameterDefinition[]>[] GetNewParametersByOriginalMethod(this MethodDefinition method, MethodGroupChange methodGroupChange)
+		{
+			var existingMethods = methodGroupChange.MethodChanges
+				.Where(x => x.ChangeType == ChangeType.Matched) // Only select methods that are still matched (I.e. that haven't been removed)
+				.Select(x => x.Method);
+
+			var methodParameterSet = new HashSet<ParameterDefinition>(method.Parameters);
+
+			return existingMethods
+				.Select(existingMethod => Tuple.Create(
+					existingMethod,
+					methodParameterSet.Except(new HashSet<ParameterDefinition>(existingMethod.Parameters), new ParameterDefinitionComparer()).ToArray()))
+				.ToArray();
+		}
+
+		private class ParameterDefinitionComparer : IEqualityComparer<ParameterDefinition>
+		{
+			public bool Equals(ParameterDefinition x, ParameterDefinition y)
+			{
+				return x.ParameterType.FullName == y.ParameterType.FullName;
+			}
+
+			public int GetHashCode(ParameterDefinition obj)
+			{
+				return obj.ParameterType.FullName.GetHashCode();
+			}
+		}
+	}
+}
