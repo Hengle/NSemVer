a987b2a6cc642d4458697b5cf88511396e110dec
andrew <devnull@localhost>
2011-10-09 19:52:51 +0100

    removed unnecessary file

--- NSemVer/Extensions.cs
+++ NSemVer/Extensions.cs
@@ -1,17 +1,27 @@
 namespace NSemVer
 {
 	using System.Globalization;
+	using System.IO;
+	using Mono.Cecil;
 	using NSemVer.Visitors;
 
 	public static class Extensions
 	{
+		public static AssemblyChanges GetChanges(this AssemblyChangesBuilder changeBuilder, Stream olderAssemblyStream, Stream newerAssemblyStream)
+		{
+			return changeBuilder.GetChanges(
+				AssemblyDefinition.ReadAssembly(olderAssemblyStream),
+				AssemblyDefinition.ReadAssembly(newerAssemblyStream));
+		}
+
 		public static void Visit(this AssemblyChanges changes, params IChangeVisitor[] visitors)
 		{
 			foreach (var changeVisitor in visitors)
 			{
 				changeVisitor.Visit(changes);
 			}
-		}		
+		}
+		
 		public static string FormatInvariant(this string s, params object[] args)
 		{
 			return string.Format(CultureInfo.InvariantCulture, s, args);
